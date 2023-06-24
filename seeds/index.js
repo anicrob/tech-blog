d0c18e8a540fe7a82e2f234eaa3422065bc9b611
goose <anrobbins18@gmail.com>
2023-06-24 15:21:21 -0400

    restructured seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -3,24 +3,16 @@ const { User, Post, Comment } = require('../models');
 const userSeeds = require('./users.json');
 const postSeeds = require('./posts.json');
 
-const seedUserTable = async () => {
+const seedTables = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userSeeds, {
     individualHooks: true,
     returning: true,
   });
+  await Post.bulkCreate(postSeeds);
 
   process.exit(0);
 };
 
-const seedPostTable = async () => {
-    await sequelize.sync({ force: true });
-  
-    await Post.bulkCreate(postSeeds);
-  
-    process.exit(0);
-  };
-
-seedUserTable();
-seedPostTable();
+seedTables();

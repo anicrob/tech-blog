e76cd94abfa77d223e520a1bc58ccf04b429137a
goose <anrobbins18@gmail.com>
2023-06-26 13:11:45 -0400

    added onto comment.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,5 +1,5 @@
 const addCommentButton = document.querySelector(".add-comment-btn");
-const commentDisplaySection = document.querySelector(
+const commentDisplaySection = document.getElementsByClassName(
   ".display-comment-section"
 );
 const sendCommentSection = document.querySelector(".send-comment-section");
@@ -8,7 +8,9 @@ const commentContent = document.querySelector(".comment-content");
 
 const addComment = async (event) => {
   addCommentButton.classList.add("hidden");
+  if(commentDisplaySection.length > 0){
   commentDisplaySection.classList.add("hidden");
+  }
   sendCommentSection.classList.remove("hidden");
 };
 
@@ -16,10 +18,11 @@ const sendComment = async (event) => {
   event.preventDefault();
   const content = commentContent.value.trim();
   const post_id = sendCommentBtn.getAttribute("data-post-id");
+  console.log(content, post_id);
   if (content) {
     const response = await fetch("/api/comments/", {
       method: "POST",
-      body: JSON.stringify({ post_id, content }),
+      body: JSON.stringify({post_id, content}),
       headers: { "Content-Type": "application/json"}
     });
     if(response.ok){
